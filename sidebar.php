123c64deeaa3f4e65d8559c3fc853a769d15ab47
PeteM <github@thrap.co.uk>
2015-01-02 17:51:08 +0000

    cleaning sidebar padding
    not sure why this was added?

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 	<?php if (is_page_template( 'page-3col.php' )) : ?>
-		<div id="sidebar1" class="sidebar large-3 medium-3 columns sidebar-3-pad" role="complementary">
+		<div id="sidebar1" class="sidebar large-3 medium-3 columns" role="complementary">
 	<?php else : ?>
-		<div id="sidebar1" class="sidebar large-4 medium-4 columns sidebar-4-pad" role="complementary">
+		<div id="sidebar1" class="sidebar large-4 medium-4 columns" role="complementary">
 	<?php endif; ?>	
 
 	<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
