d3ea6e085fcf5160612c82735ee1a6b4c3ce7a2f
PeteM <github@thrap.co.uk>
2015-01-02 17:51:53 +0000

    larger H2 on home

--- config.rb
+++ config.rb
@@ -29,8 +29,8 @@ preferred_syntax = :scss
 css_dir = "library/css" # by Compass.app 
 sass_dir = "library/scss" # by Compass.app 
 images_dir = "../images" # by Compass.app 
-output_style = :expanded # by Compass.app 
+output_style = :compressed # by Compass.app 
 relative_assets = true # by Compass.app 
-line_comments = true # by Compass.app 
+line_comments = false # by Compass.app 
 sass_options = {:debug_info=>false} # by Compass.app 
 sourcemap = false # by Compass.app 
