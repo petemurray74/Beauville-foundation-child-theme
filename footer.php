56deec22584fcbdd32d8ce0aa3c1bc30e379a251
petemurray74 <github@thrap.co.uk>
2015-06-25 21:44:12 +0100

    back to normal

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
                                     <a href="mailto:claire@beauvillearts.com">claire@beauvillearts.com</a></p>                           
 		    				</div>
                             <div class="large-4 medium-4 columns">
-                                				<p>TEST123===Société à responsabilité limitée au capital de €.8619.48<br />
+                                				<p>Société à responsabilité limitée au capital de €.8619.48<br />
                                                     No.Siret: 442 128 120 00015<br />
                                                     Code APE: 9003B</p>
                                                 <p>Les sejours de vacances organisés par <strong>Beauville Arts SARL</strong> sont déclarés auprés de la Direction Departmentale de la Cohesion Sociale et de la Protection des Populations, (DDCSPP). Nº organisateur: 047ORG0111</p>
